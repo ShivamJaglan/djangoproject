fdb311753f4ad0649f94fdca48428d3ee722172e
ShivamJaglan <shivam.jaglan3112@gmail.com>
2020-12-18 11:20:44 +0530

    added login api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,77 +4,82 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 from . import serializers
 from . import models
 from . import permissions
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
 
 # Create your views here.
 
 class HelloApiView(APIView):
-    """Test API View"""
+    """Test API View."""
 
     serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
-        """Returns a list of API View features"""
+        """Returns a list of APIView features."""
 
         an_apiview = [
-        " Uses HTTP methods as function(get, post, patch ,put, delete)",
-        "It is similar to a traditipna django view ",
-        "gives yo most control over logic",
-        "is mapped manually to urls"
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'It is similar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
         ]
 
-        return Response({'message': "hello!", 'an_apiview':an_apiview})
-    def post(self, request):
-        """create a hello message with pur name"""
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-        serializer = serializers.HelloSerializer(data= request.data)
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
 
         if serializer.is_valid():
-            name = serializer.data.get("name")
-            message = 'Hello  {0}'.format(name)
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
             return Response({'message': message})
         else:
-            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-
-
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
-        """Handles updating an object"""
-
+        """Handles updating an object."""
 
         return Response({'method': 'put'})
+
     def patch(self, request, pk=None):
         """Patch request, only updates fields provided in the request."""
 
         return Response({'method': 'patch'})
 
-
     def delete(self, request, pk=None):
-        """deletes an ob  ject"""
+        """Deletes and object."""
 
         return Response({'method': 'delete'})
-class HelloViewSet(viewsets.ViewSet):
-    """test api viewset"""
 
-    serializer_class= serializers.HelloSerializer
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
+
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
+        """Return a hello message."""
 
-        """return a hello message"""
-        a_viewset= [
-            'Uses actions(lsit, create, retrieve, update, partial_update)',
-            'Automatically maps to URLs using routers',
-            'provides more funcationality wth less code'
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code.'
         ]
 
-        return Response({'message':'Hello!' , 'a_viewset': a_viewset})
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
+    def create(self, request):
+        """Create a new hello message."""
 
-    def create(Self,request):
-        """create  anew hello messsage"""
         serializer = serializers.HelloSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -105,11 +110,24 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({'http_method': 'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handles creating, creating and updating profiels"""
-    serializer_class =  serializers.UserProfileSerializer
+    """Handles creating, creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
-    search_fields =  ('name', 'email',)
+    search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
